522e9b99be1437859c4274fa107e30df023e522d
kotkowski <kotkowski666@gmail.com>
2020-05-15 10:54:20 +0200

    Creatures v0.2
    Dodano randomizer stworzeń i modyfikatorów (jeszcze do poprawy)

--- SimpleTextRPG/SimpleTextRPG/Creatures.cs
+++ SimpleTextRPG/SimpleTextRPG/Creatures.cs
@@ -22,7 +22,7 @@ namespace SimpleTextRPG
 
         public void gainExp(int expGained)
         {
-            if (exp + expGained >= RequiredExp())
+            while(exp + expGained >= RequiredExp())
             {
                 expGained = expGained - RequiredExp();
                 level++;
@@ -34,14 +34,11 @@ namespace SimpleTextRPG
 
         }
 
-        public void drawGui(Map map, int encounter = -1, Creature enemy = null)
+        public void drawGui(Map map, int encounter = -1)
         {
             char key = '0';
             int state = 0;
-            if (enemy == null && encounter > 0)
-            {
-                enemy = new Creature("Debugger", 1, 1, 1);
-            }
+            
 
             if (x == 4 && y == 4)
             {
@@ -78,7 +75,7 @@ namespace SimpleTextRPG
                         case '1':
                             map.checkMap();
                             Console.ReadKey();
-                            drawGui(map, encounter, enemy);
+                            drawGui(map, encounter);
                             break;
                         case '2':
                             Console.ForegroundColor = ConsoleColor.Green;
@@ -101,7 +98,7 @@ namespace SimpleTextRPG
                                 case 'A': Player.y = Player.y - 1; break; // Ruch o 1 pole w górę
                                 case 'D': Player.y = Player.y + 1; break; // Ruch o 1 pole w górę
                             }
-                            drawGui(map, encounter, enemy);
+                            drawGui(map, encounter);
                             break;
                     }
                     break;
@@ -146,7 +143,7 @@ namespace SimpleTextRPG
                                 case 'A': Player.y = Player.y - 1; break; // Ruch o 1 pole w górę
                                 case 'D': Player.y = Player.y + 1; break; // Ruch o 1 pole w górę
                             }
-                            drawGui(map, encounter, enemy);
+                            drawGui(map, encounter);
                             break;
                     }
                     break;
@@ -154,7 +151,7 @@ namespace SimpleTextRPG
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("==============================");
                     Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("Wild " +  enemy.name + "(" + enemy.health + "/" + enemy.maxhealth + ") is waiting for your move!");
+                    Console.WriteLine("Wild " +  Creature.name + "(" + Creature.health + "/" + Creature.maxhealth + ") is waiting for your move!");
                     Console.WriteLine("1.Fight");
                     Console.WriteLine("2.Check");
                     Console.WriteLine("3.Defend");
@@ -170,21 +167,148 @@ namespace SimpleTextRPG
         }
     }
 
-    public class Creature
+    public static class Creature
     {
-        public string name = "Unknown Creature";
-        public int maxhealth = 20;
-        public int health = 20;
-        public int damage = 3;
-        public int defence = 0;
+        public static string name = "Unknown Creature";
+        public static int maxhealth = 20;
+        public static int health = 20;
+        public static int damage = 3;
+        public static int defence = 0;
+        public static int chargeddamage = 5*damage;
+        public static int xpreward = 5;
+        public static int goldreward = 5;
+        public static State stateofenemy = State.Idle;
 
-        public Creature(string name, int health, int damage, int defence)
+
+        public enum State
         {
-            this.name = name;
-            this.health = health;
-            this.damage = damage;
-            this.defence = defence;
+            Idle = 0,
+            Attacking = 1,
+            Preparing = 2,
+            Healing = 3
         }
+
+        public static void Randomize()
+        {
+            string modifier = "";
+            Random rnd = new Random();
+            int mod = -1;
+            int enemy = Convert.ToInt32(Math.Round((decimal)rnd.Next(0, 10)));
+            if(enemy > 5)
+            {
+                enemy = Convert.ToInt32(Math.Round((decimal)rnd.Next(0, 8)));
+                modifier = ((RandomModifier)enemy).ToString();
+                modifier = string.Concat(modifier, " ");
+                mod = enemy;
+
+            }
+
+            enemy = Convert.ToInt32(Math.Round((decimal)rnd.Next(0, 6)));
+            switch (enemy)
+            {
+                case 1:
+                    name = string.Concat(modifier + "Wolf");
+                    maxhealth = 15;
+                    health = 15;
+                    damage = 3;
+                    defence = 0;
+                    chargeddamage = 5 * damage;
+                    goldreward = 1;
+                    xpreward = 20;
+                    break;
+                case 2:
+                    name = string.Concat(modifier + "Bandit");
+                    maxhealth = 45;
+                    health = 45;
+                    damage = 10;
+                    defence = 5;
+                    chargeddamage = 5 * damage;
+                    goldreward = 20;
+                    xpreward = 60;
+                    break;
+                case 3:
+                    name = string.Concat(modifier + "Slime");
+                    maxhealth = 5;
+                    health = 5;
+                    damage = 1;
+                    defence = 20;
+                    chargeddamage = 5 * damage;
+                    goldreward = 0;
+                    xpreward = 10;
+                    break;
+                case 4:
+                    name = string.Concat(modifier + "Devil");
+                    maxhealth = 70;
+                    health = 70;
+                    damage = 10;
+                    defence = 10;
+                    chargeddamage = 5 * damage;
+                    goldreward = 600;
+                    xpreward = 200;
+                    break;
+                case 5:
+                    name = string.Concat(modifier + "Tutor");
+                    maxhealth = 20;
+                    health = 20;
+                    damage = 1;
+                    defence = 60;
+                    chargeddamage = 5 * damage;
+                    goldreward = 1200;
+                    xpreward = 200;
+                    break;
+                case 6:
+                    name = string.Concat(modifier + "Demon");
+                    maxhealth = 35;
+                    health = 35;
+                    damage = 20;
+                    defence = 5;
+                    chargeddamage = 5 * damage;
+                    goldreward = 300;
+                    xpreward = 120;
+                    break;
+                default:
+                    throw new Exception("Wrong enemy ID");
+            }
+            switch(mod)
+            {
+                case 0:
+                    damage = Convert.ToInt32(damage * 1.2);
+                    break;
+                case 1:
+                    damage = Convert.ToInt32(damage * 1.1);
+                    health = Convert.ToInt32(health * 0.8);
+                    xpreward = Convert.ToInt32(xpreward * 1.1);
+                    break;
+                case 2:
+                    damage = Convert.ToInt32(damage * 0.9);
+                    health = Convert.ToInt32(health * 1.5);
+                    xpreward = Convert.ToInt32(xpreward * 1.3);
+                    break;
+                case 3:
+                    goldreward = Convert.ToInt32(damage * 3);
+                    break;
+                case 4:
+                    damage = Convert.ToInt32(damage * 1.5);
+                    health = Convert.ToInt32(health * 0.5);
+                    xpreward = Convert.ToInt32(xpreward * 2);
+                    break;
+                case 5:
+                    damage = Convert.ToInt32(damage * 0.8);
+                    health = Convert.ToInt32(health * 0.5);
+                    xpreward = Convert.ToInt32(xpreward * 0.2);
+                    goldreward = Convert.ToInt32(goldreward * 0.2);
+                    break;
+                case 6:
+                    health = Convert.ToInt32(health * 0.8);
+                    break;
+                case 7:
+                    damage = Convert.ToInt32(health * 0.8);
+                    break;
+            }
+
+        }
+
+      
 
 
     }
